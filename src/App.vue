6274fb8047e87d0a5ab2ca3da9c11bbe8a9eee79
jannyyijie <yuaog1331@gmail.com>
2022-04-12 17:37:34 +0800

    s

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,125 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <h1>Todo</h1>
-  <input type="text" v-model="todoName" @keyup.enter="addTodo">
-  <ul>
-    <li v-for="item in todo" :key="item.id">{{item.name}}</li>
-  </ul>
-  <!-- <router-view/> -->
+  <div class="nav">
+    <div class="menu">
+      <div class="menu-title">
+        <h1 v-if="titleOpen">1111人力銀行</h1>
+        <h1 v-else>活動網站</h1>
+      </div>
+      <a-button  @click="toggleCollapsed" >
+      <MenuUnfoldOutlined v-if="collapsed" />
+      <MenuFoldOutlined v-else />
+      </a-button>
+    </div>
+    <div class="box">
+        <a-menu
+        mode="inline"
+        :inline-collapsed="collapsed"
+        v-model:openKeys="openKeys"
+        v-model:selectedKeys="selectedKeys"
+        :style="'width:'+ num * 3.3 +'%;'"
+      >
+        <a-menu-item key="1">
+          <template #icon>
+            <PieChartOutlined />
+          </template>
+          <span>Option 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <template #icon>
+            <DesktopOutlined />
+          </template>
+          <span>Option 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <template #icon>
+            <InboxOutlined />
+          </template>
+          <span>Option 3</span>
+        </a-menu-item>
+        <a-sub-menu key="sub1">
+          <template #icon>
+            <MailOutlined />
+          </template>
+          <template #title>Navigation One</template>
+          <a-menu-item key="5">Option 5</a-menu-item>
+          <a-menu-item key="6">Option 6</a-menu-item>
+          <a-menu-item key="7">Option 7</a-menu-item>
+          <a-menu-item key="8">Option 8</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template #icon>
+            <AppstoreOutlined />
+          </template>
+          <template #title>Navigation Two</template>
+          <a-menu-item key="9">Option 9</a-menu-item>
+          <a-menu-item key="10">Option 10</a-menu-item>
+          <a-sub-menu key="sub3" title="Submenu">
+            <a-menu-item key="11">Option 11</a-menu-item>
+            <a-menu-item key="12">Option 12</a-menu-item>
+          </a-sub-menu>
+        </a-sub-menu>
+      </a-menu>
+
+      <div :style="'width:'+ (100 - num * 3.3) +'%;'" >
+        <router-view  class="boxa" />
+      </div>
+    </div>
+  </div>
 </template>
-<script>
-import axios from 'axios';
-const baseURL = "http://localhost:3000/todos";
+<script >
+import { reactive, toRefs, watch, ref } from 'vue';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  PieChartOutlined,
+  MailOutlined,
+  DesktopOutlined,
+  InboxOutlined,
+  AppstoreOutlined,
+} from '@ant-design/icons-vue';
 export default ({
-  name: 'app',
-  data(){
-    return{
-      todo: [],
-      todoName:'',
+  setup() {
+    const state = reactive({
+      collapsed: false,
+      selectedKeys: ['1'],
+      openKeys: ['sub1'],
+      preOpenKeys: ['sub1'],
+    });
+
+    watch(
+      () => state.openKeys,
+      (val, oldVal) => {
+        state.preOpenKeys = oldVal;
+      },
+    );
+    const num = ref(4);
+    const titleOpen = ref(true);
+    const toggleCollapsed = () => {
+      state.collapsed = !state.collapsed;
+      state.openKeys = state.collapsed ? [] : state.preOpenKeys;
+      titleOpen.value = !titleOpen.value;
+      
+      (titleOpen.value === false) ?  num.value = 1 :  num.value = 4;
+    };
+
+    
+
+    return {
+      ...toRefs(state),
+      toggleCollapsed,
+      titleOpen,
+      num
     };
   },
-  async created(){
-    try{
-      const res = await axios.get(baseURL);
-      this.todo = res.data;
-    }catch(e){
-      console.log(e);
-    }
+  components: {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    PieChartOutlined,
+    MailOutlined,
+    DesktopOutlined,
+    InboxOutlined,
+    AppstoreOutlined,
+
   },
-  methods:{
-    async addTodo(){
-      const res = await axios.post(baseURL, {name: this.todoName});
-
-      this.todo = [...this.todo, res.data];
-      this.todoName = '';
-    }
-  }
-})
+});
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="nav">
+    <div class="menu">
+      <div class="menu-title">
+        <h1 v-if="titleOpen">1111人力銀行</h1>
+        <h1 v-else>活動網站</h1>
+      </div>
+      <a-button type="primary" @click="toggleCollapsed" >
+      <MenuUnfoldOutlined v-if="collapsed" />
+      <MenuFoldOutlined v-else />
+      </a-button>
+    </div>
+    <a-menu
+      mode="inline"
+      theme="dark"
+      :inline-collapsed="collapsed"
+      v-model:openKeys="openKeys"
+      v-model:selectedKeys="selectedKeys"
+    >
+      <a-menu-item key="1">
+        <template #icon>
+          <PieChartOutlined />
+        </template>
+        <span>Option 1</span>
+      </a-menu-item>
+      <a-menu-item key="2">
+        <template #icon>
+          <DesktopOutlined />
+        </template>
+        <span>Option 2</span>
+      </a-menu-item>
+      <a-menu-item key="3">
+        <template #icon>
+          <InboxOutlined />
+        </template>
+        <span>Option 3</span>
+      </a-menu-item>
+      <a-sub-menu key="sub1">
+        <template #icon>
+          <MailOutlined />
+        </template>
+        <template #title>Navigation One</template>
+        <a-menu-item key="5">Option 5</a-menu-item>
+        <a-menu-item key="6">Option 6</a-menu-item>
+        <a-menu-item key="7">Option 7</a-menu-item>
+        <a-menu-item key="8">Option 8</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub2">
+        <template #icon>
+          <AppstoreOutlined />
+        </template>
+        <template #title>Navigation Two</template>
+        <a-menu-item key="9">Option 9</a-menu-item>
+        <a-menu-item key="10">Option 10</a-menu-item>
+        <a-sub-menu key="sub3" title="Submenu">
+          <a-menu-item key="11">Option 11</a-menu-item>
+          <a-menu-item key="12">Option 12</a-menu-item>
+        </a-sub-menu>
+      </a-sub-menu>
+    </a-menu>
+  </div>
+</template>
+<script >
+import { reactive, toRefs, watch, ref } from 'vue';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  PieChartOutlined,
+  MailOutlined,
+  DesktopOutlined,
+  InboxOutlined,
+  AppstoreOutlined,
+} from '@ant-design/icons-vue';
+export default ({
+  setup() {
+    const state = reactive({
+      collapsed: false,
+      selectedKeys: ['1'],
+      openKeys: ['sub1'],
+      preOpenKeys: ['sub1'],
+    });
+
+    watch(
+      () => state.openKeys,
+      (val, oldVal) => {
+        state.preOpenKeys = oldVal;
+      },
+    );
+
+    const titleOpen = ref(true);
+    const toggleCollapsed = () => {
+      state.collapsed = !state.collapsed;
+      state.openKeys = state.collapsed ? [] : state.preOpenKeys;
+      titleOpen.value = false;
+    };
+
+    return {
+      ...toRefs(state),
+      toggleCollapsed,
+      titleOpen
+    };
+  },
+  components: {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    PieChartOutlined,
+    MailOutlined,
+    DesktopOutlined,
+    InboxOutlined,
+    AppstoreOutlined,
+
+  },
+});
+</script>
